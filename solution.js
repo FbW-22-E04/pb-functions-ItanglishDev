589c49583fc2c32527c7f1feef957de45c2f2bfe
chris <chrishethertonweb@gmail.com>
2022-07-14 13:46:59 +0200

    ex 1 added as function

--- solution.js
+++ solution.js
@@ -1,10 +1,16 @@
-let numArg = 4;
-
 let sumRes = 0;
-for (let i = 0; i <= numArg; i++) {
-  let sumOf = 0;
-  sumOf += i;
-  sumRes += sumOf;
-}
+// let numArg = 4;
 
+function addUp(arg) {
+  for (let i = 0; i <= arg; i++) {
+    let sumOf = 0;
+    sumOf += i;
+    sumRes += sumOf;
+  }
+}
+addUp(4);
 console.log(sumRes);
+
+let firstCube = 0;
+let secondCube = 0;
+let thirdCube = 0;
